d72ec0a1b4f8247e4f87fb7c88b6d31f14d0718f
sedmz <48542327+sedmz@users.noreply.github.com>
2021-07-09 17:16:33 +0900

    Create Solution.java

---
+++ Longest Increasing Subsequence/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+  public int lengthOfLIS(int[] nums) {
+    int max = 0;
+		
+		int[] subsequences = new int[nums.length];
+		for (int i = 0; i < nums.length - 1; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] < nums[j]) {
+					subsequences[j] = Math.max(subsequences[i] + 1, subsequences[j]);
+                    max = Math.max(max, subsequences[j]);
+				}
+			}
+		}
+        
+		return max + 1;
+	}
+}
